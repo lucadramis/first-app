85e930ee6bcc482a661038350e7c9e69bc431f90
unknown <FrontEnd@PCAU0948.formart.it>
2018-11-30 13:22:40 +0100

    progetto first app

---
+++ src/components/MyComponent.js
@@ -0,0 +1,31 @@
+import React,{ Component } from "react";
+
+class MyComponent extends Component {
+    constructor(propos){
+        super(propos);
+
+        this.state = {
+            visible: false,
+            myData:[1,2,3,4,5,6,7]
+        };
+    }
+    
+    
+    
+    render(){
+        return <div>
+            {
+                this.state.visible === true ?
+                <p>visible</p>
+                :
+                <p>not visible</p>
+            }
+            <button onClick = {this.handleClick}click</button>
+                
+                
+            </div>;
+        
+    }
+    
+}
+export default MyComponent;
